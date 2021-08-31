d6f506d1023c2b243725d24091ad860e32aca5fd
Lane, Derek G <Derek.Lane@elekta.com>
2021-08-31 14:01:01 -0500

    Add project files.

---
+++ UnoWorkList/UnoWorkList.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace UnoWorkList.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new UnoWorkList.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ UnoWorkList/UnoWorkList.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace UnoWorkList.Skia.Gtk
+{
+}
